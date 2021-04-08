0daeeaeed64cb026614d0028b3e4e4078f34ae42
niechen <niechen@xunlei.com>
2021-04-08 14:01:58 +0800

    updated redis ingo

--- go/main/main.go
+++ go/main/main.go
@@ -6,20 +6,24 @@ import (
     "net/http"
     "strconv"
     "database/sql"
-    _ "github.com/go-sql-driver/mysql"
+    "../src"
+    "encoding/json"
     "github.com/gomodule/redigo/redis"
 )
+var mysql = src.SQLConnection()
+var rediscon = src.RedisConnection()
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    con, err := sql.Open("mysql", "chen:12345678@/web")
-    if err != nil {panic(err)}
-    conn, erro := redis.Dial("tcp", "localhost:6379")
-    if erro != nil {log.Fatal(erro)}
-    
     var user_id int
     var username string
     var gender string
     var email string
+    type User struct {
+    	Username string
+    	Gender string
+    	Email string
+    }
+    
     userids, ok := r.URL.Query()["gouserid"]
     if !ok || len(userids[0]) < 1 {
         fmt.Println("No input")
@@ -28,30 +32,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
     var userid string
     userid = userids[0]
     input, err := strconv.Atoi(userid)
+    if err != nil {
+        panic(err)
+    }
     
-    id := con.QueryRow("SELECT * FROM user WHERE user_id=?", input)
+    id := mysql.QueryRow("SELECT * FROM user WHERE user_id=?", input)
     switch err := id.Scan(&user_id, &username, &gender, &email); err {
 	case sql.ErrNoRows:
   	    fmt.Println("No rows were returned!")
 	case nil:
-	    if_exist, err := redis.Int(conn.Do("EXISTS", userid))
+	    if_exist, err := redis.Int(rediscon.Do("EXISTS", userid))
+	    fmt.Println(if_exist)
 	    if err != nil {fmt.Println("redis error")}
 	    if if_exist == 1{
 	        fmt.Println("Found the key in redis.")
-	    	output, err := redis.String(conn.Do("GET", userid))
+	    	output, err := rediscon.Do("GET", userid)
 	    	if err != nil {log.Fatal(err)}
-	    	fmt.Println(output)
+	    	var o User
+	    	bytes, _ := output.([]byte)
+	    	erro := json.Unmarshal(bytes, &o)
+	    	if erro != nil {
+		    panic(err)
+	        }
+	    	fmt.Println("Username: " + o.Username + ", Gender: " + o.Gender + ", Email: " + o.Email)
 	    } else {
 	        fmt.Println("Didn't find the key in redis. Will insert key.")
-	        output := "Username: " + username + ", Gender: " + gender + ", Email: " + email
-	        fmt.Println(output)
-  	        conn.Do("SET", userid, output)
+	        output := User {
+	        	Username: username,
+	        	Gender: gender,
+	        	Email: email,
+	        }
+	        o, err:= json.Marshal(output)
+	        if err != nil {
+		    fmt.Println("error:", err)
+	        }
+	        fmt.Println("Username: " + username + ", Gender: " + gender + ", Email: " + email)
+  	        rediscon.Do("SET", userid, o)
   	    }
 	default:
   	    panic(err)
     }
-    defer con.Close();
-    defer conn.Close()
 }
 
 func main() {
